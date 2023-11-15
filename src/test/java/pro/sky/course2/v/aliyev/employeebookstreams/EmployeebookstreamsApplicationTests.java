a97ff2e1f522cdf811bd84c6cce34df3368067cb
VGAliyev <v.aliev84@yandex.ru>
2023-11-15 21:11:13 +0400

    Initial commit

---
+++ src/test/java/pro/sky/course2/v/aliyev/employeebookstreams/EmployeebookstreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.v.aliyev.employeebookstreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeebookstreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
